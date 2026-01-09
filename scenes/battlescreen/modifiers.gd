6a33796ff06894591344db90c47179b2ea1558f1
Will Hager <111020616+willhager@users.noreply.github.com>
2026-01-09 11:59:54 -0500

    a bunch of stuff

--- scenes/battlescreen/modifiers.gd
+++ scenes/battlescreen/modifiers.gd
@@ -1,7 +1,8 @@
 extends Label
 
 func _on_mouse_entered() :
-	print("mouse entered modifier")
 	tooltip_manager.show_tooltip("modifier", "lmao test text test test test test test test test test test test test test", Vector2(500, 500))
 	
+func _on_mouse_exited() :
+	tooltip_manager.hide_tooltip()
 	
--- scenes/tooltip/tooltip.gd
+++ scenes/tooltip/tooltip.gd
@@ -3,26 +3,23 @@ extends PopupPanel
 @onready var title_label = $Panel/MarginContainer/VBoxContainer/Title
 @onready var text_label = $Panel/MarginContainer/VBoxContainer/Text
 
-
 var hide_timer: Timer
 
 func _ready():
 	hide_timer = Timer.new()
 	hide_timer.one_shot = true
-	hide_timer.wait_time = 0.05  # small delay, enough to catch fast switches
+	hide_timer.wait_time = 0.05
 	add_child(hide_timer)
 	hide_timer.timeout.connect(_on_hide_timeout)
+	print("min size =", get_min_size())
 
 func show_tooltip(titleText: String, text: String, global_pos: Vector2):
 	title_label.text = titleText
 	text_label.text = text
 	
 	hide_timer.stop()
+	await get_tree().process_frame
 	popup()
-	size = Vector2.ZERO
-	call_deferred("_finish_popup", global_pos)
-
-func _finish_popup(global_pos: Vector2):
 	position = global_pos
 	
 func request_hide() :
